41895007237989a1da3bdb4b785034b4f52d7aec
sillypoise <somewhatjose@gmail.com>
2024-04-03 16:17:05 -0500

    fix conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,14 @@
--- Or with configuration
 return {
-  "projekt0n/github-nvim-theme",
-  -- "kepano/flexoki-neovim",
-  -- opts = {
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   name = "flexoki",
-  -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "flexoki-dark",
-      colorscheme = "github_dark_high_contrast",
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "jq" },
+      sh = { "shfmt" },
     },
   },
 }
